22a5fce639242d49d5e01fe287eb2c4389de58f1
Matahari Ramadhan <ramadhan.matahari24@gmail.com>
2022-12-18 23:51:50 +0200

    refactor main.py & gitignore, and create readme.md & requirements.txt

---
+++ README.md
@@ -0,0 +1,21 @@
+# OCR ARABIC
+
+This is python program to convert arabic pdf or img to text.
+
+## Prerequisite
+
+1. Install Tesseract
+2. Download the arabic trained model
+
+## Usage
+
+1. You can make new virtual environment with command : `python3 -m venv env`, and then activate it with: `source env/bin/activate`
+2. Install all the dependencies:
+   ```bash
+   pip install -r requirements.txt
+   ```
+3. Then execute the main.py file:
+   ```bash
+   python main.py
+   ```
+4. Provide full path of pdf file and full path of folder that you want to save the result to.
--- main.py
+++ main.py
@@ -1,37 +1,59 @@
+import sys
+import os
+import tempfile
 from PIL import Image
 import pytesseract as pt
-import os
-      
+from pdf2image import convert_from_path
+
+
 def main():
-    # path for the folder for getting the raw images
-    path = input("Enter path folder to convert:")
-  
-    # path for the folder for getting the output
-    tempPath = input("Enter path folder to save:")
+    pdf_path = ""
+    result_path = ""
 
-    option = "-l ara"
-  
-    # iterating the images inside the folder
-    for imageName in os.listdir(path):
+    # Asking path to pdf that want to be converted
+    while pdf_path == "":
+        pdf_path = input("Enter full path to pdf: ")
+        if pdf_path == "":
+            exit = input(
+                'Please enter the full path of pdf!, wanna exit?(y/N): ')
+            if exit == "y" or exit == "Y":
+                print('Exiting!')
+                sys.exit()
 
-        inputPath = os.path.join(path, imageName)
-        img = Image.open(inputPath)
-  
-        # applying ocr using pytesseract for python
-        text = pt.image_to_string(img, config=option)
-  
-        # for removing the .jpg from the imagePath
-        # imagePath = imagePath[0:-4]
-  
-        imageName = imageName[-7:]
+    # Asking path to save the result
+    while result_path == "":
+        result_path = input("Enter the path to save the result: ")
+        if result_path == "":
+            exit = input(
+                'Please enter the full path to save the result!, wanna exit?(y/N): ')
+            if exit == "y" or exit == "Y":
+                print('Exiting!')
+                sys.exit()
 
-        fullTempPath = os.path.join(tempPath, imageName+".txt")
-        print(text)
-  
-        # saving the  text for every image in a separate .txt file
-        file1 = open(fullTempPath, "w", encoding="utf-16")
-        file1.write(text)
-        file1.close() 
-  
+    # initialize temorary directory
+    with tempfile.TemporaryDirectory() as tempDir:
+        # convert pdf to image
+        convert_from_path(pdf_path, output_folder=tempDir)
+
+        # iterating the images inside the folder
+        for imageName in os.listdir(tempDir):
+            img = Image.open(os.path.join(tempDir, imageName))
+
+            # convert img to string (arabic) with pytesseract
+            text = pt.image_to_string(img, config="-l ara")
+
+            # take 7 last character as imageName exluding .ppm extension
+            imageName = imageName[-7:-4]
+
+            fullTempPath = os.path.join(result_path, imageName+".txt")
+
+            # saving the  text for every image in a separate .txt file
+            file = open(fullTempPath, "w", encoding="utf-16")
+            file.write(text)
+            file.close()
+
+    print('Done! Your text is ready now')
+
+
 if __name__ == '__main__':
     main()
