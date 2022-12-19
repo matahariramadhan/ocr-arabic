a02e6248ddf24ef92d10a9a6467033e88edac0da
Matahari Ramadhan <ramadhan.matahari24@gmail.com>
2022-12-19 19:24:00 +0200

    create OCR class and refactor main.py

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 import sys
-import os
-import tempfile
-from PIL import Image
-import pytesseract as pt
-from pdf2image import convert_from_path
-from zipfile import ZipFile
+from ocr import OCR
 
 
 def get_user_input(message: str, error_message='Please provide the asked input') -> str:
@@ -36,36 +31,8 @@ def main():
     result_path = get_user_input(
         "Enter the path to save the result: ", "Please enter the path to save the result")
 
-    # initialize temorary directory
-    with tempfile.TemporaryDirectory() as tempDir:
-        # convert pdf to image
-        convert_from_path(pdf_path, output_folder=tempDir)
-
-        # iterating the images inside the folder
-        for imageName in os.listdir(tempDir):
-            img = Image.open(os.path.join(tempDir, imageName))
-
-            # convert img to string (arabic) with pytesseract
-            text = pt.image_to_string(img, config="-l ara")
-
-            # take 7 last character as imageName exluding .ppm extension
-            imageName = imageName.split('-')[-1:][0]
-            imageName = imageName[:-4]
-
-            fullTempPath = os.path.join(tempDir, imageName+".txt")
-
-            # saving the  text for every image in a separate .txt file
-            with open(fullTempPath, "w", encoding="utf-16") as file:
-                file.write(text)
-
-        # zip all text result in temp directory
-        zipObj = ZipFile(result_path + "Result.zip", 'w')
-        for file in os.listdir(tempDir):
-            file_extension = "q"+file[:]
-            if file_extension[-4:] == '.txt':
-                zipObj.write(os.path.join(tempDir, file), file)
-
-        zipObj.close()
+    ocr = OCR()
+    ocr.convert_file_to_zip(file_path=pdf_path, output_folder=result_path)
 
     print('Done! Your text is ready now')
 
---
+++ ocr.py
@@ -0,0 +1,50 @@
+import os
+import tempfile
+import pytesseract as pt
+from zipfile import ZipFile
+from pdf2image import convert_from_path
+from PIL import Image
+
+
+class OCR:
+    '''OCR that can convert pdf or image to text'''
+
+    def __init__(self) -> None:
+        pass
+
+    def convert_file_to_zip(self, file_path: str, output_folder: str) -> None:
+        '''
+        Convert pdf to zip
+
+        Convert pdf to text and archive the result to Result.zip then store it in output folder
+        '''
+        # initialize temorary directory
+        with tempfile.TemporaryDirectory() as tempDir:
+            # convert pdf to image
+            convert_from_path(file_path, output_folder=tempDir)
+
+            # iterating the images inside the folder
+            for imageName in os.listdir(tempDir):
+                img = Image.open(os.path.join(tempDir, imageName))
+
+                # convert img to string (arabic) with pytesseract
+                text = pt.image_to_string(img, config="-l ara")
+
+                # take 7 last character as imageName exluding .ppm extension
+                imageName = imageName.split('-')[-1:][0]
+                imageName = imageName[:-4]
+
+                fullTempPath = os.path.join(tempDir, imageName+".txt")
+
+                # saving the  text for every image in a separate .txt file
+                with open(fullTempPath, "w", encoding="utf-16") as file:
+                    file.write(text)
+
+            # zip all text result in temp directory
+            zipObj = ZipFile(output_folder + "Result.zip", 'w')
+            for file in os.listdir(tempDir):
+                file_extension = "q"+file[:]
+                if file_extension[-4:] == '.txt':
+                    zipObj.write(os.path.join(tempDir, file), file)
+
+            zipObj.close()
